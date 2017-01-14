1e3dee8470ea70ed8370709a6d990d6933349b08
wkhuahuo <wkhuahuo@163.com>
2017-01-14 21:22:58 +0800

    联系request

--- webDemo/hello.py
+++ webDemo/hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!<h1>'
+    #return '<h1>Hello World!<h1>'
+    user_agent = request.headers.get('User-Agent')
+    meth = request.method
+    header = request.headers
+    return '<p>Your browser is %s</p><p>method = %s</p><p>headers = %s</p>' % (user_agent,meth, header)
+
 
 @app.route('/user/<name>')
 def user(name):
